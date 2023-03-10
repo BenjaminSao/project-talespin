55001009a43871487fc56c1ea90c8f4a951c9a64
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2023-03-10 15:39:19 -0500

    REFACTORING: Prettier

--- talespin-api/index.js
+++ talespin-api/index.js
@@ -7,12 +7,12 @@ app.use(bodyParser.json());
 
 // Add Endpoints Here!
 app.get("/", async (req, res) => {
-    return res.status(200).json({
-        message: "Hi!",
-    });
+  return res.status(200).json({
+    message: "Hi!",
+  });
 });
 
 app.listen(PORT, (e) => {
-    if (e) console.log(e);
-    else console.log("Server is running on Port:", PORT);
+  if (e) console.log(e);
+  else console.log("Server is running on Port:", PORT);
 });
