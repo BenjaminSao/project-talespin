4ccf96b43950af520334d7bb940375613bd22250
johnson_su <johnsonsu111@gmail.com>
2023-03-30 17:41:03 -0400

    MERGE: Fix index.js

--- talespin-api/src/index.js
+++ talespin-api/src/index.js
@@ -6,21 +6,13 @@ import { imageRouter } from "./routers/image_router.js";
 import { log } from "./middlewares/log_middleware.js";
 
 import cors from "cors";
-import bodyParser from "body-parser";
-import { getUserId } from "./utils/authentication_util.js";
-import { checkJwt } from "./middlewares/authentication_middleware.js";
 
-
-import { sequelize } from "./datasource.js";
 import { sequelizeSetup } from "./configs/sequelize_config.js";
 import { errorHandling } from "./middlewares/error_handling_middleware.js";
 
 // Express Setup
 const PORT = process.env.API_URL || 3001;
 export const app = express();
-
-app.use("/api/story", storyRouter);
-app.use("/api/images", imageRouter);
 
 _serverSetup();
 _runServer();
@@ -29,7 +21,13 @@ async function _serverSetup() {
   app.use(bodyParser.json());
   app.use(cors());
   app.use(log);
+
+  // Setup routers
+  app.use("/api/stories", storyRouter);
+  app.use("/api/images", imageRouter);
+
   app.use(errorHandling);
+
   await sequelizeSetup();
 }
 
